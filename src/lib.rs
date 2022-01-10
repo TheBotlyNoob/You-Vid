31e6bb5625bfe20414f7b1f49702bb34094f99b7
TheBotlyNoob <jj@jj.is-a.dev>
2022-01-09 21:56:48 -0500

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern "C" {
+  fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+  console_error_panic_hook::set_once();
+
+  alert("Hello, you-vid!");
+}
