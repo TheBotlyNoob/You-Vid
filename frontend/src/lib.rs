9065167d0191d6c30bc60661726ddff716777e8f
Jay Jackson <jj@jj.is-a.dev>
2022-01-14 15:20:26 +0000

    error handling

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -29,6 +29,11 @@ fn app() -> Html {
 mod prelude {
   pub use super::components::*;
   pub use common::prelude::*;
+  pub use gloo::console;
+  pub use std::{
+    error::Error,
+    sync::{Arc, Mutex},
+  };
   pub use wasm_bindgen::{prelude::*, JsCast};
   pub use wasm_bindgen_futures::{spawn_local, JsFuture};
   pub use web_sys::Response;
--- frontend/src/routes/login.rs
+++ frontend/src/routes/login.rs
@@ -1,33 +1,58 @@
 use crate::prelude::*;
 use gloo::utils::window;
 
-#[derive(Default)]
+#[derive(Default, Clone)]
 pub struct Login {
   error: Option<String>,
 }
 
 pub enum Msg {
   Login(String, String),
+  SetError(Option<String>),
+  Nothing,
 }
 
 impl Component for Login {
   type Message = Msg;
   type Properties = ();
 
-  fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+  fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+    let link = ctx.link();
+
     match msg {
       Msg::Login(_email, _password) => {
-        let res = window()
-          .fetch_with_str_and_init("/api/login", web_sys::RequestInit::new().method("POST"))
-          .then(&Closure::wrap(Box::new(|res: JsValue| {
-            let res = res.dyn_into::<Response>().unwrap();
-          }) as Box<dyn FnMut(JsValue)>))
-          .catch(&Closure::wrap(Box::new(|err: JsValue| {
-            self.error = err.as_string();
-          }) as Box<dyn FnMut(JsValue)>>);
+        link.send_future(async {
+          let res = JsFuture::from(
+            window()
+              .fetch_with_str_and_init("/api/login", web_sys::RequestInit::new().method("POST")),
+          )
+          .await;
+
+          console::error!(format!("Hello {:#?}", res));
+
+          if let Err(error) = res {
+            return Msg::SetError(error.as_string());
+          }
+
+          let res = res.unwrap().dyn_into::<Response>().unwrap();
+
+          if !res.ok() {
+            return Msg::SetError(Some(format!("Got status code {}", res.status())));
+          }
+
+          Msg::Nothing
+        });
 
         true
       }
+
+      Msg::SetError(error) => {
+        self.error = error;
+
+        true
+      }
+
+      Msg::Nothing => false,
     }
   }
 
